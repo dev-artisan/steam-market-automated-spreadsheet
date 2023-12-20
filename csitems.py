5bee7400ba3a9358bca832cab2969ecad6db1fa3
Omar Helal <omar@celtisaustralis.com>
2023-12-20 18:39:14 +0300

    Add encoding when opening files

--- csitems.py
+++ csitems.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 def get_items(filename):
     items = []
-    with open(filename, "r") as txt_items:
+    with open(filename, "r", encoding="utf8") as txt_items:
         for line in txt_items:
             line = line.strip("\n")
             if line:
@@ -17,7 +17,7 @@ def get_items(filename):
 
 def get_history(name):
     try:
-        with open(f"{name}.history.json", "r") as file:
+        with open(f"{name}.history.json", "r", encoding="utf8") as file:
             history = json.loads(file.read())
     except FileNotFoundError as e:
         history = {}
@@ -41,18 +41,19 @@ def clean_up_history(history, items):
 def write_history(name, history):
     print("Writing History")
     print(json.dumps(history))
-    with open(f"{name}.history.json", "w") as file:
+    with open(f"{name}.history.json", "w", encoding="utf8") as file:
         file.write(json.dumps(history))
 
 
 def write_output_csv(name, history):
     print("Writing CSV")
-    with open(f"{name}.output.csv", "w") as file:
+    with open(f"{name}.output.csv", "w", encoding="utf8") as file:
         file.write("Date,Item,Price")
         for date, date_data in history.items():
             for item, item_data in date_data.items():
-                output = f"{date},{item},{item_data.get('median_price')}\n"
-                file.write(output)
+                if item_data.get("success"):
+                    output = f"{date},\"{item}\",{item_data.get('median_price')}\n"
+                    file.write(output)
 
 
 def main(filename, currency):
@@ -68,7 +69,7 @@ def main(filename, currency):
 
     for item in items:
         # Sleep it to avoid rate limiting and returning nothing
-        time.sleep(3)
+        time.sleep(2)
 
         result = steammarket.get_csgo_item(item, currency=currency)
         data = history.get(isodate, {})
